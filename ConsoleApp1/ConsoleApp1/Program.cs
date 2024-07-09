9515210da7ed19d53f648ea9b043ae277c72b82e
Santosh Dahal <sirishsds@gmail.com>
2024-07-09 14:47:17 +0545

    Variables

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-var name = "Test";
-
-Console.WriteLine("Hello " + name);
+﻿using System;
+namespace DataTypesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables of different data types
+            int myInt = 25;
+            double myDouble = 3.14;
+            char myChar = 'A';
+            string myString = "Hello, World!";
+            bool myBool = true;
+            // Print the variables to the console
+            Console.WriteLine("Integer: " + myInt);
+            Console.WriteLine("Double: " + myDouble);
+            Console.WriteLine("Char: " + myChar);
+            Console.WriteLine("String: " + myString);
+            Console.WriteLine("Boolean: " + myBool);
+        }
+    }
+}
