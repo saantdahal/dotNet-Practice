da7a1f9a967a960bdfde9c152cbdbc13952eee60
Santosh Dahal <sirishsds@gmail.com>
2024-07-10 19:02:58 +0545

    Basic Operations

---
+++ ConsoleApp/BasicOperations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BasicOperations
+{
+    class Program
+    {
+        static void Main(String [] args)
+        {
+            // Declare varibles of different data types
+
+            int myInt = 50;
+            String name = "ram";
+            Boolean myBul = true;
+            double myDou=50.3;
+            char myChar= 'c';
+
+            // Print the variable to console
+
+            System.Console.WriteLine("Integer: "+ myInt);
+            System.Console.WriteLine("String: "+name);
+            System.Console.WriteLine("Boolean: "+myBul);
+            System.Console.WriteLine("Double: "+myDou);
+            System.Console.WriteLine("Character: "+myChar);
+        }
+    }
+}
