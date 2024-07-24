32579cea8c6408ba04ae9ae719c01d8e1b389cf1
Santosh Dahal <sirishsds@gmail.com>
2024-07-24 17:29:00 +0545

    Classes simple program

---
+++ ConsoleApp/classExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classExample
+{
+    internal class Program
+    {
+        // Create a Class 'car'
+        class Car
+        {
+            public String carName;
+            public int currSpeed;
+
+            //create a methood 'printState'
+            public void printState()
+            {
+                Console.WriteLine($"{carName} is going {currSpeed} Kmph");
+            }
+
+            public void speedUp(int delta)
+            {
+                currSpeed += delta;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with c# objects");
+            Car myCar = new Car();
+            myCar.currSpeed = 20;
+            myCar.carName = "Tesla";
+            for (int i = 0; i < 20; i++)
+            {
+                myCar.speedUp(10);
+                myCar.printState();
+            }
+        }
+    }
+}
