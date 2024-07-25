ff95cfb5c0e4f86a94990deaadeefe17663a1a82
Santosh Dahal <sirishsds@gmail.com>
2024-07-25 07:58:47 +0545

    Cosntructor

---
+++ ConsoleApp/Constructor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+
+    {
+        public class Person
+        {
+            public string name;
+            public int age;
+
+            // Deafault Constructor
+            public Person()
+            {
+                name = "Unkwoun";
+                age = 0;
+            }
+            // Parameterized Constructor
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+
+
+
+            static void Main(string[] args)
+
+            {
+                //Console.WriteLine("Enter your name:");
+                //string name = Console.ReadLine();
+                
+                //Console.WriteLine("Enter your age:");
+                //int age = int.Parse(Console.ReadLine());
+
+
+                Person p1 = new Person();
+                Console.WriteLine($"Person 1 Name: {p1.name} Person 1 Age: {p1.age}");
+
+
+            }
+        }
+    }
+}
