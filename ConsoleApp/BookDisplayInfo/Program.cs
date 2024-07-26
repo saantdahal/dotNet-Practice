b83668a97d30c0ed71671b229cfd90a78c7e726f
Santosh Dahal <sirishsds@gmail.com>
2024-07-26 06:07:00 +0545

    Book Info

---
+++ ConsoleApp/BookDisplayInfo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookDisplayInfo
+{
+
+        internal class Program
+        {
+            class Book
+            {
+                String title;
+                String author;
+                int pages;
+                double price;
+
+                // creating a Constructor
+                public Book()
+                {
+                    title = "C#";
+                    author = "microsoft";
+                    pages = 0;
+                }
+
+                // Create a parameterized Constructor
+                public Book(String title, String author, int pages)
+                {
+                    this.title = title;
+                    this.author = author;
+                    this.pages = pages;
+                }
+
+                public void BookInfo()
+                {
+                    Console.WriteLine($"Title : {title} Author : {author} Pages : {pages}");
+                }
+            }
+            static void Main(string[] args)
+            {
+                Book book = new Book();
+                book.BookInfo();
+
+
+
+            }
+        }
+    }
+
+
+
