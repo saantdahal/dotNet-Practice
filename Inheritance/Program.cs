3dfd34b7136f223045ef967a25e7ef714687a4cd
Santosh Dahal <sirishsds@gmail.com>
2024-08-03 13:29:01 +0545

    Simple Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        public class Employee
+        {
+            protected string firstName="Ram";
+            protected string lastName="Sharma";
+            protected int employeeID=42;
+            protected float salary=1500.2f;
+
+
+        }
+
+        public class Manager : Employee
+        {
+            string department= "HOD";
+
+            public void displayMethod(){
+                System.Console.WriteLine("======================================");
+                System.Console.WriteLine($"|  First Name of the employee: {firstName}   |");
+                System.Console.WriteLine($"|  Last Name of the employee: {lastName} |");
+                System.Console.WriteLine($"|  Employee ID: {employeeID}                   |");
+                System.Console.WriteLine($"|  Salary of the employee: {salary}    |");
+                System.Console.WriteLine($"|  Department of the employee: {department}   |");
+                System.Console.WriteLine("======================================");
+                
+            }
+        }
+        static void Main(String[] args)
+        {
+          Manager m1 = new Manager();
+          m1.displayMethod();
+        }
+    }
+}
